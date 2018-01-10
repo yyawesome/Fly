86b96a18b81bf2961992370ac915bdb0787059c2
linjia <linjia@fanruan.com>
2018-01-11 00:14:42 +0800

    Fix bug && Add test DataFrame constructor with DIct

--- FlyDailyInfo.py
+++ FlyDailyInfo.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+import tushare as ts
 import numpy as np
 import pandas as pd
-from pandas import Series
+from pandas import Series, DataFrame
 
 DAILY_LIMIT_P = 9.8  # 涨幅，实际上涨停应大于此数值
 DAY_COL_KEY_HIGH = 'high'
@@ -18,11 +19,21 @@ def get_daily_limit_along_factor(df):
     date_series = Series(date_col_df)
     # 这个series包含了每个date中的一字板个数
     date_num_series = date_series.value_counts()
-    date_num_df = {DAY_COL_KEY_DATE: date_num_series.index,
-                   DAY_COL_KEY_HLA_NUM: date_num_series.values}
+    date_num_df = DataFrame({DAY_COL_KEY_DATE: date_num_series.index,
+                             DAY_COL_KEY_HLA_NUM: date_num_series.values})
+    print(date_num_df)
     # 默认根据date作为两个表的key值进行连接合并
     return pd.merge(df, date_num_df)
 
 
 if __name__ == '__main__':
     print("hello")
+    # df = ts.get_hist_data('603283').reset_index()
+    # df2 = ts.get_hist_data('002907').reset_index()
+    # df3 = ts.get_hist_data('300664').reset_index()
+    # df4 = ts.get_hist_data('603655').reset_index()
+    # df5 = ts.get_hist_data('300433').reset_index()
+    # df6 = ts.get_hist_data('600903').reset_index()
+    # dfa = pd.concat([df, df2, df3, df4, df5, df6])
+    # f = get_daily_limit_along_factor(dfa)
+    # print(f)
