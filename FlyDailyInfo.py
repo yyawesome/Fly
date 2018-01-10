fb6d826d8d67a0ce10298cf35771b5d92d6e626b
linjia <linjia@fanruan.com>
2018-01-10 23:51:53 +0800

    修改出入参，对全日期全股票行情的df进行统计，并增加一列结果到原有df=》df2，结果返回df2

--- FlyDailyInfo.py
+++ FlyDailyInfo.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python
-import tushare as ts
 import numpy as np
+import pandas as pd
+from pandas import Series
 
 DAILY_LIMIT_P = 9.8  # 涨幅，实际上涨停应大于此数值
 DAY_COL_KEY_HIGH = 'high'
 DAY_COL_KEY_LOW = 'low'
 DAY_COL_KEY_P = 'p_change'
+DAY_COL_KEY_DATE = 'date'
+DAY_COL_KEY_HLA_NUM = 'high_limit_along_num'
 
 
-def get_daily_limit_along_number(date=None):
-    _day_all_df = ts.get_day_all(date)
-    _number = _day_all_df[np.logical_and(_day_all_df[DAY_COL_KEY_HIGH] == _day_all_df[DAY_COL_KEY_LOW],
-                                         _day_all_df[DAY_COL_KEY_P] > DAILY_LIMIT_P)].shape[0]
-    return _number
+# 根据一字板涨停条件抽取出一个df[date] 生成series 然后进行count，在将series根据date count 添加到原df
+def get_daily_limit_along_factor(df):
+    date_col_df = df[np.logical_and(df[DAY_COL_KEY_HIGH] == df[DAY_COL_KEY_LOW],
+                                    df[DAY_COL_KEY_P] > DAILY_LIMIT_P)][DAY_COL_KEY_DATE]
+    date_series = Series(date_col_df)
+    # 这个series包含了每个date中的一字板个数
+    date_num_series = date_series.value_counts()
+    date_num_df = {DAY_COL_KEY_DATE: date_num_series.index,
+                   DAY_COL_KEY_HLA_NUM: date_num_series.values}
+    # 默认根据date作为两个表的key值进行连接合并
+    return pd.merge(df, date_num_df)
 
 
 if __name__ == '__main__':
-    daily_limit_along_number = get_daily_limit_along_number()
-    print(daily_limit_along_number)
+    print("hello")
