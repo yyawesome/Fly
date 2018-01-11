ed1e0f8a670e9e7b71c94e34e264cb45a8eb7441
linjia <linjia@fanruan.com>
2018-01-11 17:30:56 +0800

    更新功能为：统计非一字板的个数 一字板判定（涨停并且开盘涨幅大于8%）

--- FlyDailyInfo.py
+++ FlyDailyInfo.py
@@ -4,24 +4,19 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
-DAILY_LIMIT_P = 9.8  # 涨幅，实际上涨停应大于此数值
-DAY_COL_KEY_HIGH = 'high'
-DAY_COL_KEY_LOW = 'low'
-DAY_COL_KEY_P = 'p_change'
-DAY_COL_KEY_DATE = 'date'
-DAY_COL_KEY_HLA_NUM = 'high_limit_along_num'
 
-
+# 统计非一字板的个数 一字板判定（涨停并且开盘涨幅大于8%）
 # 根据一字板涨停条件抽取出一个df[date] 生成series 然后进行count，在将series根据date count 添加到原df
 def get_daily_limit_along_factor(df):
-    date_col_df = df[np.logical_and(df[DAY_COL_KEY_HIGH] == df[DAY_COL_KEY_LOW],
-                                    df[DAY_COL_KEY_P] > DAILY_LIMIT_P)][DAY_COL_KEY_DATE]
-    date_series = Series(date_col_df)
-    # 这个series包含了每个date中的一字板个数
+    limit_df = df[np.logical_and(df['open'] >= 8,
+                                 df['p_change'] > 9.8)]
+    rest_df = df.drop(limit_df)['date']
+    date_series = Series(rest_df)
+    # 这个series包含了每个date中的非一字板个数
     date_num_series = date_series.value_counts()
-    date_num_df = DataFrame({DAY_COL_KEY_DATE: date_num_series.index,
-                             DAY_COL_KEY_HLA_NUM: date_num_series.values})
-    # print(date_num_df)
+    date_num_df = DataFrame({'date': date_num_series.index,
+                             'daily_limit_along_factor': date_num_series.values})
+    print(date_num_df)
     # 默认根据date作为两个表的key值进行连接合并
     return pd.merge(df, date_num_df)
 
@@ -36,4 +31,3 @@ if __name__ == '__main__':
     # df6 = ts.get_hist_data('600903').reset_index()
     # dfa = pd.concat([df, df2, df3, df4, df5, df6])
     # f = get_daily_limit_along_factor(dfa)
-    # print(f)
